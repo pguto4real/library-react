c0229cb68db997271cb34d093ec462d734dc8fc6
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-06 13:44:10 -0500

    add half star to component ui folder

---
+++ src/components/ui/FontAwesomeHalfStar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function FontAwesomeHalfStar(key) {
+  return <FontAwesomeIcon icon="star-half-alt" key={key} />;
+}
