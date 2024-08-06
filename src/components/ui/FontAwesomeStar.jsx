034e924d65fd8ce8c7367e50e2f98714c14b02ec
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-06 13:45:33 -0500

    add star to component ui folder

---
+++ src/components/ui/FontAwesomeStar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function FontAwesomeStar(key) {
+  return <FontAwesomeIcon icon="star" key={key} />;
+}
