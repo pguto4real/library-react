2b60eadbac2062ade00dc8cae955bb2b6d36e7dc
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-06 19:22:00 -0500

    add home page and add route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Nav from "./components/nav";
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home}/>
+        <Route path="/books" component={Books}/>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
