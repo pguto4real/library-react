f2f776393d2f095424bcb79263eeacb0a6e8f99d
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-07 21:45:48 -0500

    Add AFarrow left ui component

---
+++ src/components/ui/FontAwesomeArrowLeft.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export default function FontAwesomeArrowLeft(key) {
+  return <FontAwesomeIcon icon="arrow-left" key={key} />;
+}
